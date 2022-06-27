caf3c216207fd8ace6536e90055064711b3260e5
markg <mark.grangaard@gmail.com>
2022-06-27 13:16:54 -0500

    add modularization

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const generatePage = (userName, githubName) => {
-    return `
-    Name: ${userName}
-    Github: ${githubName}
-    `;
-};
-console.log(generatePage(name, github));
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-// console.log(profileDataArgs);
-
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('==============');
-//     profileDataArr.forEach((profileItem)=> {
-//         console.log(profileItem)
-//     });
-// };
-
-// printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
